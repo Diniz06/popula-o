5fa055b243ab9ad1fe8f6f9d2a418a8e4fe27fea
Diniz06 <hugodiniz039@gmail.com>
2025-06-06 17:33:25 -0300

    Add files via upload

---
+++ ciração de lista tropa da mari.py
@@ -0,0 +1,14 @@
+s1 = float(input('Qual o salario da primeira pessoa?'))
+s2 = float(input('Qual o salario da segunda pessoa?'))
+s3 = float(input('Qual o salario da terceira pessoa?'))
+s4= float(input('Qual o salario da  quarta pessoa?'))
+
+f1 = int(input('Qual o numero de filhos da primeira pessoa?'))
+f2 = int(input('Qual o numero de filhos da segunda pessoa?'))
+f3 = int(input('Qual o numero de filhos da terceira pessoa?'))
+f4 = int(input('Qual o numero de filhos da quarta pessoa?'))
+
+print('A media salarial da popoulaçãlo é', (s1 + s2 + s3 + s4) / 4)
+
+print('A media salarial da popoulaçãlo é', (f1 + f2 + f3 + f4) / 4)
+
